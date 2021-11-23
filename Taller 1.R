8d17aeb4a2aca4a61d57007416f1c2092ce2c0b2
Gianfranco <gianestevezruiz@hotmail.com>
2021-11-23 18:30:25 -0500

    Clase 23-11-2021

--- Taller 1.R
+++ Taller 1.R
@@ -7,8 +7,12 @@ median(saber_pro$RC)
 saber_pro2<-data.frame(saber_pro)
 
 saber_pro2$PG<-rowMeans(saber_pro2[,5:9])
+saber_pro2$PG2<-rowSums(saber_pro2[,5:9])
+
+
 rowMeans(saber_pro)
 
+#
 summary(saber_pro2)
 
 typeof(saber_pro2)
@@ -17,7 +21,29 @@ hist(saber_pro$GENERO)
 
 ggplot2
 
+#Media = 150, Desviación std = 30 FUN reescale
 
+aggregate(PG~PERIODO,data = saber_pro2, FUN = mean)
+
+
+#Histograma por año
+hist(saber_pro2[saber_pro2$PERIODO == '2018', 'PG'])
+
+
+
+aggregate(PG~PERIODO,data = saber_pro2, FUN = max)
+
+
+
+#Histograma
 barplot(with(saber_pro2, table(GENERO)))
 
+
+
 install.packages('ggplot2')
+
+colMax <- function(saber_pro2) sapply(saber_pro2, max, na.rm = TRUE)
+
+
+#Filtrado por año
+subset(saber_pro2, PERIODO=='2014' )
